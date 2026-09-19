8fea389741284388c5efb3c02bbb7528c5729d84
Rishabh Singh <rishabhsingh9093@gmail.com>
2026-09-19 11:47:46 +0000

    Class Methods

---
+++ classmethods.py
@@ -0,0 +1,34 @@
+class Employee:
+    raise_amt = 1.05
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+
+    def full_name(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def raise_sal(self):
+        return self.pay * 1.06
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp):
+        first, last, pay = emp.split('-')
+        return cls(first, last, pay)
+
+
+    
+Emp1 = Employee('Rishabh', 'Singh', 10000)
+emp = 'Rishabh-Singh-10000'
+new_emp = Employee.from_string(emp)
+print(new_emp.first)
+Emp1.set_raise_amt(1.06)
+print(Emp1.raise_amt)
+
+
